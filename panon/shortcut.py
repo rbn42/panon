112033485ee2d0b802897fc482c36ecfd8ef236b
rbn42 <bl100@students.waikato.ac.nz>
2018-02-24 22:33:30 +0800

    ignore decoding error

--- panon/shortcut.py
+++ panon/shortcut.py
@@ -34,7 +34,7 @@ class Shortcut(Gtk.EventBox):
 
     def tick(self):
         text = subprocess.check_output(self.shortcut['auto-command'], shell=True)
-        text = text.decode()
+        text = text.decode(errors='ignore')
         text = text.strip()
         self.label.set_text(text)
         return True
