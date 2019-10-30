fab75e5fab70fc00c4936e2a0fb73232ad7fa059
rbn42 <bl100@students.waikato.ac.nz>
2019-10-30 13:59:11 +0800

    Load glsl file in runtime

--- kde/install.sh
+++ kde/install.sh
@@ -1,8 +1,8 @@
-python build.py
 
 rm ../panon/__pycache__ -r
 
 kpackagetool5 -t Plasma/Applet --install plasmoid
 kpackagetool5 -t Plasma/Applet --upgrade plasmoid
 
+rm ./panon.plasmoid
 zip -r panon.plasmoid ./plasmoid 
--- kde/plasmoid/contents/ui/utils.js
+++ kde/plasmoid/contents/ui/utils.js
@@ -1,8 +1,15 @@
-function get_scripts_root() {
+function get_root() {
     var p_ui = plasmoid.file("ui")
     p_ui = p_ui.split('/')
     p_ui.pop(-1)
-    p_ui.push('scripts')
     return p_ui.join('/')
 }
 
+function get_scripts_root() {
+    return get_root() + '/scripts'
+}
+
+function read_shader(name) {
+    var path = get_root() + '/shaders/' + name
+    return 'cat "' + path + '"'
+}
