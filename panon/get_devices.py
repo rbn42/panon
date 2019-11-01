03a2f835490db74301b945d73c161a419ce85a48
rbn42 <bl100@students.waikato.ac.nz>
2019-11-01 23:41:33 +0800

    Remove devices with no input channel

--- panon/get_devices.py
+++ panon/get_devices.py
@@ -3,6 +3,10 @@ import json
 p = pyaudio.PyAudio()
 l = []
 for i in range(p.get_device_count()):
-    l.append(p.get_device_info_by_index(i))
+    obj=p.get_device_info_by_index(i)
+    if obj['maxInputChannels']<1:
+        # Remove devices with no input channel
+        continue
+    l.append(obj)
 s = json.dumps(l)
 print(s)
