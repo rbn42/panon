385a4391173eadd4149d234a5d78ceecfc0c6033
rbn42 <rbn_42@outlook.com>
2020-02-20 21:02:35 +0800

    Package translations into the plasmoid file.

--- makepackage.sh
+++ makepackage.sh
@@ -3,27 +3,21 @@
 #Verify the existence of third party files before packaging.
 if [ -f "third_party/hsluv-glsl/hsluv-glsl.fsh" ];then
 
-    # plasmoid
+    # Remove caches
     rm ./plasmoid/contents/scripts/__pycache__/ -r
     rm ./plasmoid/contents/scripts/*/__pycache__/ -r
     rm ./plasmoid/contents/scripts/*/*/__pycache__/ -r
     rm ./plasmoid/contents/scripts/*/*/*/__pycache__/ -r
     rm ./panon.plasmoid
-    zip -r panon.plasmoid ./plasmoid 
 
     # i18n
-    cd ./translations
     mkdir build
     cd build 
-    cmake ..
-    rm ./locale -r
-    make install DESTDIR=./locale
-    cd locale
-    NAME=../../../i18n.tgz
-    rm $NAME
-    tar czvf $NAME *
-    # Extract
-    # tar xzvf $NAME -C ~/.local/share/locale
+    cmake ../translations
+    make install DESTDIR=../plasmoid/contents/locale
+    cd ..
+
+    zip -r panon.plasmoid ./plasmoid 
 
 else
     echo "Cannot find third party files."
